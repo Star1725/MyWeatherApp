56b86ec081c215935cd93fb9fb7ba63c07eb293b
Sergey <starovoytov1725@gmail.com>
2020-11-16 16:46:46 +0300

    отключено логгирование

--- app/src/main/java/com/geekbrains/myweatherapp/Logger.java
+++ app/src/main/java/com/geekbrains/myweatherapp/Logger.java
@@ -2,6 +2,6 @@ package com.geekbrains.myweatherapp;
 
 public class Logger {
     public static final String TAG = "myLog";
-    public static boolean VERBOSE = true;
+    public static boolean VERBOSE = false;
     public static boolean DEBUG = VERBOSE && true;
 }
